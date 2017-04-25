6fa9489143d78219693a907df87fcbd534cc9145
KEVIN JOHNSON <kjjohnson@uc.cl>
2017-04-24 20:26:13 -0400

    subidos exceles

--- entrega 3/politica_inventario_dia.py
+++ entrega 3/politica_inventario_dia.py
@@ -1,4 +1,4 @@
-import numpy.random as n
+import numpy.random as nr
 import matplotlib.pyplot as plt
 import math
 from random import normalvariate, triangular, randint, uniform
@@ -22,6 +22,7 @@ def imprimir_inv(mes, invB, invK, invP):
     print(string)
 
 def SIMULAR_AÑO(imprimir):
+    inventarios_historicos = [[], [], []]
     inv_B = 60000
     inv_K = 60000
     inv_P = 55000
@@ -29,6 +30,7 @@ def SIMULAR_AÑO(imprimir):
     multas_B = 0
     multas_K = 0
     multas_P = 0
+
     if imprimir:
         print("                Bright Koala Paper")
         print("1 de Enero:", round(inv_B), round(inv_K), round(inv_P))
@@ -46,6 +48,10 @@ def SIMULAR_AÑO(imprimir):
         inv_K += (29.4 * 3.2 * 30)
         inv_P += (36.9 * 3.6 * 30)
         multas_B,multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[0].append(round(inv_B))
+        inventarios_historicos[1].append(round(inv_K))
+        inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Enero", inv_B, inv_K, inv_P)
 
@@ -63,6 +69,10 @@ def SIMULAR_AÑO(imprimir):
         inv_K += (29.4 * 4.5 * 30)
         inv_P += (36.9 * 4.5 * 30)
         multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[0].append(round(inv_B))
+        inventarios_historicos[1].append(round(inv_K))
+        inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Febrero", inv_B, inv_K, inv_P)
 
@@ -75,11 +85,13 @@ def SIMULAR_AÑO(imprimir):
         inv_B -= dema_b
         inv_B += (28.43 * 4.7 * 30)
         multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[0].append(round(inv_B))
     for i in range(19):
         dema_b = normalvariate(4000, 100)
         inv_B -= dema_b
         inv_B += (28.43 * 7 * 30) #Maxima Capacidad, que en esta temporada es en promedio 7
         multas_B,multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[0].append(round(inv_B))
 
     #PARA KOALA
     for i in range(16):
@@ -87,11 +99,13 @@ def SIMULAR_AÑO(imprimir):
         inv_K -= dema_k
         inv_K += (29.4 * 4.5 * 30)
         multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[1].append(round(inv_K))
     for i in range(14):
         dema_k = normalvariate(4000, 200)
         inv_K -= dema_k
         inv_K += (29.4 * 7 * 30)   #Máxima Capacidad, que en esta temporada es en promedio 7
         multas_B,multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[1].append(round(inv_K))
 
     #PARA PAPER
     for i in range(18):
@@ -99,11 +113,14 @@ def SIMULAR_AÑO(imprimir):
         inv_P -= dema_p
         inv_P += (36.9 * 4.5 * 30)
         multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[2].append(round(inv_P))
     for i in range(12):
         dema_p = triangular(4500, 5000, 5500)
         inv_P -= dema_p
         inv_P += (36.9 * 7 * 30)   #Máxima Capacidad, que en esta temporada es en promedio 7
         multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Marzo", inv_B, inv_K, inv_P)
 
@@ -121,6 +138,10 @@ def SIMULAR_AÑO(imprimir):
         inv_K += (29.4 * 4 * 30)  #Máxima Capacidad
         inv_P += (36.9 * 4 * 30)  #Máxima Capacidad
         multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[0].append(round(inv_B))
+        inventarios_historicos[1].append(round(inv_K))
+        inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Abril", inv_B, inv_K, inv_P)
 
@@ -138,6 +159,10 @@ def SIMULAR_AÑO(imprimir):
         inv_K += (29.4 * 4 * 30)   #Máxima Capacidad
         inv_P += (36.9 * 4 * 30)   #Máxima Capacidad
         multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[0].append(round(inv_B))
+        inventarios_historicos[1].append(round(inv_K))
+        inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Mayo", inv_B, inv_K, inv_P)
 
@@ -155,6 +180,10 @@ def SIMULAR_AÑO(imprimir):
         inv_K += (29.4 * 4.55 * 30)
         inv_P += (36.9 * 4.5 * 30)
         multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+        inventarios_historicos[0].append(round(inv_B))
+        inventarios_historicos[1].append(round(inv_K))
+        inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Diciembre", inv_B, inv_K, inv_P)
 
@@ -164,11 +193,11 @@ def SIMULAR_AÑO(imprimir):
         print("Koala:", multas_K)
         print("Paper:", multas_P, "\n")
 
-    return (round(inv_B), round(inv_K), round(inv_P), multas_B, multas_K, multas_P)
+    return (round(inv_B), round(inv_K), round(inv_P), multas_B, multas_K, multas_P, inventarios_historicos)
 
-n=10
 
-replicas = [SIMULAR_AÑO(0) for i in range(n)]
+n=1
+replicas = [SIMULAR_AÑO(imprimir=0) for i in range(n)]
 resultados = list(zip(*replicas))
 
 
@@ -178,6 +207,7 @@ paper = resultados[2]
 MB = resultados[3]
 MK = resultados[4]
 MP = resultados[5]
+inventarios_historicos = resultados[6]
 allMultas = MB + MK + MP
 
 print("Inventarios al final del 1er año, luego de {} réplicas.".format(n))
@@ -185,3 +215,10 @@ print("        min   prom  max    Tmultas")
 print("Bright:", end=" "); print(min(bright), end=" "); print(prom(bright), end=" "); print(max(bright), end="   "); print(sum(MB), end="\n")
 print("Koala:", end="  "); print(min(koala), end=" "); print(prom(koala), end=" "); print(max(koala), end="   "); print(sum(MK), end="\n")
 print("Paper:", end="  "); print(min(paper), end=" "); print(prom(paper), end=" "); print(max(paper), end="   "); print(sum(MP), end="\n")
+
+replica1 = inventarios_historicos[0]
+bright_r1 = replica1[0]
+print(min(bright_r1))
+plt.plot(range(1,361), bright_r1)
+plt.plot(range(1, 361), [26884 for i in range(360)])
+plt.show()
--- entrega 3/politica_inventario_hora.py
+++ entrega 3/politica_inventario_hora.py
@@ -22,6 +22,7 @@ def imprimir_inv(mes, invB, invK, invP):
     print(string)
 
 def SIMULAR_AÑO(imprimir):
+    inventarios_historicos = [[], [], []]
     inv_B = 60000
     inv_K = 60000
     inv_P = 55000
@@ -34,7 +35,7 @@ def SIMULAR_AÑO(imprimir):
         print("1 de Enero:", round(inv_B), round(inv_K), round(inv_P))
 
     ###############################################################################################3
-    #Enero
+    #Enero 2017
     for i in range(30):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
@@ -47,12 +48,16 @@ def SIMULAR_AÑO(imprimir):
             inv_K -= dema_k/24
             inv_P -= dema_p/24
             multas_B,multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+            inventarios_historicos[1].append(round(inv_K))
+            inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Enero", inv_B, inv_K, inv_P)
 
     ###############################################################################################3
 
-    #Febrero
+    #Febrero 2017
     for i in range(30):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
@@ -65,11 +70,15 @@ def SIMULAR_AÑO(imprimir):
             inv_K -= dema_k/24
             inv_P -= dema_p/24
             multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+            inventarios_historicos[1].append(round(inv_K))
+            inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Febrero", inv_B, inv_K, inv_P)
 
     ###############################################################################################3
-    #Marzo
+    #Marzo 2017
 
     #PARA BRIGHT
     for i in range(11):
@@ -78,6 +87,7 @@ def SIMULAR_AÑO(imprimir):
             inv_B += (28.43 * 4.7 * 30) / 24
             inv_B -= dema_b/24
             multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
 
     for i in range(19):
         dema_b = normalvariate(4000, 100)
@@ -85,6 +95,7 @@ def SIMULAR_AÑO(imprimir):
             inv_B += (28.43 * 7 * 30) / 24  #Maxima Capacidad, que en esta temporada es en promedio 7
             inv_B -= dema_b/24
             multas_B,multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
 
     #PARA KOALA
     for i in range(16):
@@ -93,6 +104,7 @@ def SIMULAR_AÑO(imprimir):
             inv_K += (29.4 * 4.5 * 30) / 24
             inv_K -= dema_k/24
             multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[1].append(round(inv_K))
 
     for i in range(14):
         dema_k = normalvariate(4000, 200)
@@ -100,6 +112,7 @@ def SIMULAR_AÑO(imprimir):
             inv_K += (29.4 * 7 * 30) / 24  # Máxima Capacidad, que en esta temporada es en promedio 7
             inv_K -= dema_k/24
             multas_B,multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[1].append(round(inv_K))
 
     #PARA PAPER
     for i in range(18):
@@ -108,6 +121,7 @@ def SIMULAR_AÑO(imprimir):
             inv_P += (36.9 * 4.5 * 30) / 24
             inv_P -= dema_p/24
             multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[2].append(round(inv_P))
 
     for i in range(12):
         dema_p = triangular(4500, 5000, 5500)
@@ -115,12 +129,14 @@ def SIMULAR_AÑO(imprimir):
             inv_P += (36.9 * 7 * 30) / 24  # Máxima Capacidad, que en esta temporada es en promedio 7
             inv_P -= dema_p/24
             multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Marzo", inv_B, inv_K, inv_P)
 
     ###############################################################################################3
 
-    #Abril
+    #Abril 2017
     for i in range(30):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
@@ -133,12 +149,16 @@ def SIMULAR_AÑO(imprimir):
             inv_K -= dema_k/24
             inv_P -= dema_p/24
             multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+            inventarios_historicos[1].append(round(inv_K))
+            inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Abril", inv_B, inv_K, inv_P)
 
     ###############################################################################################3
 
-    #Mayo
+    #Mayo 2017
     for i in range(30):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
@@ -151,12 +171,16 @@ def SIMULAR_AÑO(imprimir):
             inv_K -= dema_k/24
             inv_P -= dema_p/24
             multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+            inventarios_historicos[1].append(round(inv_K))
+            inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Mayo", inv_B, inv_K, inv_P)
 
     ###############################################################################################3
 
-    #Meses de Junio a Diciembre
+    #Meses de Junio a Diciembre 2017
     for i in range(210):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
@@ -169,6 +193,180 @@ def SIMULAR_AÑO(imprimir):
             inv_K -= dema_k/24
             inv_P -= dema_p/24
             multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+            inventarios_historicos[1].append(round(inv_K))
+            inventarios_historicos[2].append(round(inv_P))
+
+    if imprimir:
+        imprimir_inv("Diciembre", inv_B, inv_K, inv_P)
+
+    if imprimir:
+        print("                Bright Koala Paper")
+        print("1 de Enero:", round(inv_B), round(inv_K), round(inv_P))
+
+    ###############################################################################################3
+    #Enero 2018
+    for i in range(30):
+        dema_b = normalvariate(4000, 100)
+        dema_k = normalvariate(4000, 200)
+        dema_p = triangular(4500, 5000, 5500)
+        for j in range(24):
+            inv_B += (28.43 * 4.7 * 30) / 24
+            inv_K += (29.4 * 4.55 * 30) / 24
+            inv_P += (36.9 * 4.5 * 30) / 24
+            inv_B -= dema_b/24
+            inv_K -= dema_k/24
+            inv_P -= dema_p/24
+            multas_B,multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+            inventarios_historicos[1].append(round(inv_K))
+            inventarios_historicos[2].append(round(inv_P))
+
+    if imprimir:
+        imprimir_inv("Enero", inv_B, inv_K, inv_P)
+
+    ###############################################################################################3
+
+    #Febrero 2018
+    for i in range(30):
+        dema_b = normalvariate(4000, 100)
+        dema_k = normalvariate(4000, 200)
+        dema_p = triangular(4500, 5000, 5500)
+        for j in range(24):
+            inv_B += (28.43 * 4.7 * 30)/24
+            inv_K += (29.4 * 4.55 * 30)/24
+            inv_P += (36.9 * 4.5 * 30)/24
+            inv_B -= dema_b/24
+            inv_K -= dema_k/24
+            inv_P -= dema_p/24
+            multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+            inventarios_historicos[1].append(round(inv_K))
+            inventarios_historicos[2].append(round(inv_P))
+
+    if imprimir:
+        imprimir_inv("Febrero", inv_B, inv_K, inv_P)
+
+    ###############################################################################################3
+    #Marzo 2018
+
+    #PARA BRIGHT
+    for i in range(11):
+        dema_b = normalvariate(4000, 100)
+        for j in range(24):
+            inv_B += (28.43 * 4.7 * 30) / 24
+            inv_B -= dema_b/24
+            multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+
+    for i in range(19):
+        dema_b = normalvariate(4000, 100)
+        for j in range(24):
+            inv_B += (28.43 * 7 * 30) / 24  #Maxima Capacidad, que en esta temporada es en promedio 7
+            inv_B -= dema_b/24
+            multas_B,multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+
+    #PARA KOALA
+    for i in range(16):
+        dema_k = normalvariate(4000, 200)
+        for j in range(24):
+            inv_K += (29.4 * 4.5 * 30) / 24
+            inv_K -= dema_k/24
+            multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[1].append(round(inv_K))
+
+    for i in range(14):
+        dema_k = normalvariate(4000, 200)
+        for j in range(24):
+            inv_K += (29.4 * 7 * 30) / 24  # Máxima Capacidad, que en esta temporada es en promedio 7
+            inv_K -= dema_k/24
+            multas_B,multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[1].append(round(inv_K))
+
+    #PARA PAPER
+    for i in range(18):
+        dema_p = triangular(4500, 5000, 5500)
+        for j in range(24):
+            inv_P += (36.9 * 4.5 * 30) / 24
+            inv_P -= dema_p/24
+            multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[2].append(round(inv_P))
+
+    for i in range(12):
+        dema_p = triangular(4500, 5000, 5500)
+        for j in range(24):
+            inv_P += (36.9 * 7 * 30) / 24  # Máxima Capacidad, que en esta temporada es en promedio 7
+            inv_P -= dema_p/24
+            multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[2].append(round(inv_P))
+
+    if imprimir:
+        imprimir_inv("Marzo", inv_B, inv_K, inv_P)
+
+    ###############################################################################################3
+
+    #Abril 2018
+    for i in range(30):
+        dema_b = normalvariate(4000, 100)
+        dema_k = normalvariate(4000, 200)
+        dema_p = triangular(4500, 5000, 5500)
+        for j in range(24):
+            inv_B += (28.43 * 4 * 30) / 24  # Máxima Capacidad, que en esta temporada es en promedio 4
+            inv_K += (29.4 * 4 * 30) / 24  # Máxima Capacidad
+            inv_P += (36.9 * 4 * 30) / 24  # Máxima Capacidad
+            inv_B -= dema_b/24
+            inv_K -= dema_k/24
+            inv_P -= dema_p/24
+            multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+            inventarios_historicos[1].append(round(inv_K))
+            inventarios_historicos[2].append(round(inv_P))
+
+    if imprimir:
+        imprimir_inv("Abril", inv_B, inv_K, inv_P)
+
+    ###############################################################################################3
+
+    #Mayo 2018
+    for i in range(30):
+        dema_b = normalvariate(4000, 100)
+        dema_k = normalvariate(4000, 200)
+        dema_p = triangular(4500, 5000, 5500)
+        for j in range(24):
+            inv_B += (28.43 * 4 * 30)/24  #Máxima Capacidad, que en esta temporada es en promedio 4
+            inv_K += (29.4 * 4 * 30)/24   #Máxima Capacidad
+            inv_P += (36.9 * 4 * 30)/24   #Máxima Capacidad
+            inv_B -= dema_b/24
+            inv_K -= dema_k/24
+            inv_P -= dema_p/24
+            multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+            inventarios_historicos[1].append(round(inv_K))
+            inventarios_historicos[2].append(round(inv_P))
+
+    if imprimir:
+        imprimir_inv("Mayo", inv_B, inv_K, inv_P)
+
+    ###############################################################################################3
+
+    #Meses de Junio a Diciembre 2018
+    for i in range(210):
+        dema_b = normalvariate(4000, 100)
+        dema_k = normalvariate(4000, 200)
+        dema_p = triangular(4500, 5000, 5500)
+        for j in range(24):
+            inv_B += (28.43 * 4.7 * 30)/24
+            inv_K += (29.4 * 4.55 * 30)/24
+            inv_P += (36.9 * 4.5 * 30)/24
+            inv_B -= dema_b/24
+            inv_K -= dema_k/24
+            inv_P -= dema_p/24
+            multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
+            inventarios_historicos[0].append(round(inv_B))
+            inventarios_historicos[1].append(round(inv_K))
+            inventarios_historicos[2].append(round(inv_P))
+
     if imprimir:
         imprimir_inv("Diciembre", inv_B, inv_K, inv_P)
 
@@ -178,23 +376,51 @@ def SIMULAR_AÑO(imprimir):
         print("Koala:", multas_K)
         print("Paper:", multas_P, "\n")
 
-    return (round(inv_B), round(inv_K), round(inv_P), multas_B, multas_K, multas_P)
+    return (round(inv_B), round(inv_K), round(inv_P), multas_B, multas_K, multas_P, inventarios_historicos)
 
-n=10
-replicas = [SIMULAR_AÑO(0) for i in range(n)]
-resultados = list(zip(*replicas))
+def graficar_inventarios_replica(j, inventarios_historicos):
+
+    replicaJ = inventarios_historicos[j-1]
+    bright_r1 = replicaJ[0]
+    koala_r1 = replicaJ[1]
+    paper_r1 = replicaJ[2]
+
+    minimo_b_1 = min(bright_r1)
+    minimo_k_1 = min(koala_r1)
+    minimo_p_1 = min(paper_r1)
+    #print(minimo_b_1, minimo_k_1, minimo_p_1)
+
+    plt.plot(range(1,2*360*24+1), bright_r1, color="orange")
+    plt.plot(range(1,2*360*24+1), koala_r1, color="blue")
+    plt.plot(range(1,2*360*24+1), paper_r1, color="red")
+    #plt.plot(range(1, 361*24), [minimo_b_1 for i in range(360*24)])
+    plt.show()
+
+def SIMULACION_FINAL(n):
+    n=1
+    replicas = [SIMULAR_AÑO(0) for i in range(n)]
+    resultados = list(zip(*replicas))
+
+    bright_end_year = resultados[0]
+    koala_end_year = resultados[1]
+    paper_end_year = resultados[2]
+    MB = resultados[3]
+    MK = resultados[4]
+    MP = resultados[5]
+    inventarios_historicos = resultados[6]
+    historic_bright = [inventarios_historicos[j][0] for j in range(n)]
+    print(len(historic_bright))
+    allMultas = MB + MK + MP
+
+    """
+    print("Estadistica al final del 1er año, luego de {} réplicas.".format(n))
+    print("        min   prom  max    Tmultas")
+    print("Bright:", end=" "); print(min(bright_end_year), end=" "); print(prom(bright_end_year), end=" "); print(max(bright_end_year), end="   "); print(sum(MB), end="\n")
+    print("Koala:", end="  "); print(min(koala_end_year), end=" "); print(prom(koala_end_year), end=" "); print(max(koala_end_year), end="   "); print(sum(MK), end="\n")
+    print("Paper:", end="  "); print(min(paper_end_year), end=" "); print(prom(paper_end_year), end=" "); print(max(paper_end_year), end="   "); print(sum(MP), end="\n")
+    """
+
+    graficar_inventarios_replica(1, inventarios_historicos)
 
 
-bright = resultados[0]
-koala = resultados[1]
-paper = resultados[2]
-MB = resultados[3]
-MK = resultados[4]
-MP = resultados[5]
-allMultas = MB + MK + MP
-
-print("Inventarios al final del 1er año, luego de {} réplicas.".format(n))
-print("        min   prom  max    Tmultas")
-print("Bright:", end=" "); print(min(bright), end=" "); print(prom(bright), end=" "); print(max(bright), end="   "); print(sum(MB), end="\n")
-print("Koala:", end="  "); print(min(koala), end=" "); print(prom(koala), end=" "); print(max(koala), end="   "); print(sum(MK), end="\n")
-print("Paper:", end="  "); print(min(paper), end=" "); print(prom(paper), end=" "); print(max(paper), end="   "); print(sum(MP), end="\n")
+SIMULACION_FINAL(10)
