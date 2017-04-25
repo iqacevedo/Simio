581b2d62ce57aab16bcbdc57b4f06b7ecd52d8a8
KEVIN JOHNSON <kjjohnson@uc.cl>
2017-04-25 01:55:01 -0400

    subidos los excels

--- entrega 3/politica_inventario_hora.py
+++ entrega 3/politica_inventario_hora.py
@@ -36,7 +36,7 @@ def SIMULAR_AÑO(imprimir):
 
     ###############################################################################################3
     #Enero 2017
-    for i in range(30):
+    for i in range(31):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
         dema_p = triangular(4500, 5000, 5500)
@@ -58,7 +58,7 @@ def SIMULAR_AÑO(imprimir):
     ###############################################################################################3
 
     #Febrero 2017
-    for i in range(30):
+    for i in range(28):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
         dema_p = triangular(4500, 5000, 5500)
@@ -81,7 +81,7 @@ def SIMULAR_AÑO(imprimir):
     #Marzo 2017
 
     #PARA BRIGHT
-    for i in range(11):
+    for i in range(13):
         dema_b = normalvariate(4000, 100)
         for j in range(24):
             inv_B += (28.43 * 4.7 * 30) / 24
@@ -89,7 +89,7 @@ def SIMULAR_AÑO(imprimir):
             multas_B, multas_K, multas_P = calcular_multas(inv_B, inv_K, inv_P, multas_B, multas_K, multas_P)
             inventarios_historicos[0].append(round(inv_B))
 
-    for i in range(19):
+    for i in range(18):
         dema_b = normalvariate(4000, 100)
         for j in range(24):
             inv_B += (28.43 * 7 * 30) / 24  #Maxima Capacidad, que en esta temporada es en promedio 7
@@ -98,7 +98,7 @@ def SIMULAR_AÑO(imprimir):
             inventarios_historicos[0].append(round(inv_B))
 
     #PARA KOALA
-    for i in range(16):
+    for i in range(17):
         dema_k = normalvariate(4000, 200)
         for j in range(24):
             inv_K += (29.4 * 4.5 * 30) / 24
@@ -115,7 +115,7 @@ def SIMULAR_AÑO(imprimir):
             inventarios_historicos[1].append(round(inv_K))
 
     #PARA PAPER
-    for i in range(18):
+    for i in range(19):
         dema_p = triangular(4500, 5000, 5500)
         for j in range(24):
             inv_P += (36.9 * 4.5 * 30) / 24
@@ -159,7 +159,7 @@ def SIMULAR_AÑO(imprimir):
     ###############################################################################################3
 
     #Mayo 2017
-    for i in range(30):
+    for i in range(31):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
         dema_p = triangular(4500, 5000, 5500)
@@ -181,7 +181,7 @@ def SIMULAR_AÑO(imprimir):
     ###############################################################################################3
 
     #Meses de Junio a Diciembre 2017
-    for i in range(210):
+    for i in range(214):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
         dema_p = triangular(4500, 5000, 5500)
@@ -206,7 +206,7 @@ def SIMULAR_AÑO(imprimir):
 
     ###############################################################################################3
     #Enero 2018
-    for i in range(30):
+    for i in range(31):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
         dema_p = triangular(4500, 5000, 5500)
@@ -228,7 +228,7 @@ def SIMULAR_AÑO(imprimir):
     ###############################################################################################3
 
     #Febrero 2018
-    for i in range(30):
+    for i in range(28):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
         dema_p = triangular(4500, 5000, 5500)
@@ -251,7 +251,7 @@ def SIMULAR_AÑO(imprimir):
     #Marzo 2018
 
     #PARA BRIGHT
-    for i in range(11):
+    for i in range(12):
         dema_b = normalvariate(4000, 100)
         for j in range(24):
             inv_B += (28.43 * 4.7 * 30) / 24
@@ -268,7 +268,7 @@ def SIMULAR_AÑO(imprimir):
             inventarios_historicos[0].append(round(inv_B))
 
     #PARA KOALA
-    for i in range(16):
+    for i in range(17):
         dema_k = normalvariate(4000, 200)
         for j in range(24):
             inv_K += (29.4 * 4.5 * 30) / 24
@@ -285,7 +285,7 @@ def SIMULAR_AÑO(imprimir):
             inventarios_historicos[1].append(round(inv_K))
 
     #PARA PAPER
-    for i in range(18):
+    for i in range(19):
         dema_p = triangular(4500, 5000, 5500)
         for j in range(24):
             inv_P += (36.9 * 4.5 * 30) / 24
@@ -329,7 +329,7 @@ def SIMULAR_AÑO(imprimir):
     ###############################################################################################3
 
     #Mayo 2018
-    for i in range(30):
+    for i in range(31):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
         dema_p = triangular(4500, 5000, 5500)
@@ -351,7 +351,7 @@ def SIMULAR_AÑO(imprimir):
     ###############################################################################################3
 
     #Meses de Junio a Diciembre 2018
-    for i in range(210):
+    for i in range(214):
         dema_b = normalvariate(4000, 100)
         dema_k = normalvariate(4000, 200)
         dema_p = triangular(4500, 5000, 5500)
@@ -390,9 +390,17 @@ def graficar_inventarios_replica(j, inventarios_historicos):
     minimo_p_1 = min(paper_r1)
     #print(minimo_b_1, minimo_k_1, minimo_p_1)
 
-    plt.plot(range(1,2*360*24+1), bright_r1, color="orange")
-    plt.plot(range(1,2*360*24+1), koala_r1, color="blue")
-    plt.plot(range(1,2*360*24+1), paper_r1, color="red")
+    #plt.plot(range(1,2*365*24+1), bright_r1, color="orange")
+    plt.plot(range(1,2*365*24+1), koala_r1, color="blue")
+    #plt.plot(range(1,2*365*24+1), paper_r1, color="red")
+    plt.plot([2160 for i in range(10)], [20000 + i * 4500 for i in range(10)], color = "black")
+    plt.plot([3600 for i in range(10)], [20000 + i * 4500 for i in range(10)], color="black")
+    plt.plot([8640 for i in range(10)], [20000 + i*4500 for i in range(10)], color = "red")
+    plt.plot([10800 for i in range(10)], [20000 + i * 4500 for i in range(10)],color="black")
+    plt.plot([12240 for i in range(10)], [20000 + i * 4500 for i in range(10)],color="black")
+    plt.ylim(20000, 65000)
+
+    plt.title("Bright = Orange,    Koala = Blue,    Paper = Red")
     #plt.plot(range(1, 361*24), [minimo_b_1 for i in range(360*24)])
     plt.show()
 
