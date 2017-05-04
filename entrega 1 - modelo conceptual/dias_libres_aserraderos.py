cda9235919ffd28551b2ff9a99b0337572455b96
KEVIN JOHNSON <kjjohnson@uc.cl>
2017-05-03 21:23:29 -0400

    cambiada los dias libres

--- entrega 1 - modelo conceptual/dias_libres_aserraderos.py
+++ entrega 1 - modelo conceptual/dias_libres_aserraderos.py
@@ -1,16 +1,22 @@
 from random import choice, randint
 
 archivo = open("datos_dias_libres.txt", "w")
+dias = [0,0,0,1,2,3,4,5,6]
 for i in range(1,101):
-    dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"] + ["Domingo"]*3
-    cuantos_dias = choice([1, 2])
+    aux = [0,0,0,0,0,0,0]
+    cuantos_dias = choice([1,1,1,2])
     if cuantos_dias == 2:
         dia_1 = choice(dias)
         dia_2 = choice(dias)
         while dia_1 == dia_2:
             dia_2 = choice(dias)
-        fila = "[{}-{}]".format(dia_1, dia_2)
+        aux[dia_1] = 1
+        aux[dia_2] = 1
     else:
-        fila = "[{}]".format(choice(dias))
-    archivo.write("{}-.{}\n".format(i, fila))
+        dia_1 = choice(dias)
+        aux[dia_1] = 1
+
+    aux = list(map(str, aux))
+    archivo.write(",".join(aux) + "\n")
+    
 archivo.close()
